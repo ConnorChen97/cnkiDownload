d8a4ad509e2e482058995caf6ab660a7518f7898
Connor Chen <connorchen.job@gmail.com>
2022-11-05 01:08:55 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# cnkiDownload
+1. 安装nodejs
+   https://nodejs.org/en/download/
+
+2. 安装包
+npm i
+
+3. 下载对应chrome版本的驱动并添加到环境变量
+https://registry.npmmirror.com/binary.html?path=chromedriver/
+
+> 查看chrome版本：chrome://version/
+
+4. 启动程序
+npm run start
---
+++ main.js
@@ -0,0 +1,62 @@
+const { Builder, By } = require('selenium-webdriver');
+const { papers } = require('./paper.json');
+const { USERNAME, PASSWORD } = require('./user.json');
+
+const sleep = async (time) => {
+    await new Promise(res => setTimeout(res, time));
+}
+
+const login = async (driver) => {
+    await driver.get('https://ersp.lib.whu.edu.cn/s/net/cnki/www/G.https');
+    await driver.findElement(By.id('username')).sendKeys(USERNAME);
+    await driver.findElement(By.id('password')).sendKeys(PASSWORD);
+    await sleep(500);
+    await driver.findElement(By.className('auth_login_btn primary full_width')).click();
+    await sleep(3000);
+}
+
+const download = async (driver, paper) => {
+    await driver.findElement(By.id('txt_SearchText')).clear();
+    await sleep(500);
+    await driver.findElement(By.id('txt_SearchText')).sendKeys(paper);
+    await sleep(500);
+    
+    await driver.findElement(By.className('search-btn')).click();
+    await sleep(3000);
+    
+    await driver.findElement(By.xpath('//*[@id="gridTable"]/table/tbody/tr[1]/td[2]/a/font')).click();
+    await sleep(1000);
+    
+    const windows = await driver.getAllWindowHandles();
+    await driver.switchTo().window(windows[1])
+    
+    await sleep(500);
+    await driver.findElement(By.id('pdfDown')).click();
+    await sleep(500);
+    await driver.close();
+    await driver.switchTo().window(windows[0])
+    
+    await sleep(500);
+    await driver.navigate().back();
+}
+
+(async () => {
+    let driver;
+    try {
+        driver = await new Builder().forBrowser('chrome').build();
+        await login(driver);
+
+        for (const paper of papers) {
+            console.log(`download paper: ${paper}...`);
+            await download(driver, paper);
+            await sleep(500);
+            console.log(`download paper: ${paper} done`);
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        console.log('script down');
+        // 关闭浏览器
+        await driver.quit();
+    }
+})();
